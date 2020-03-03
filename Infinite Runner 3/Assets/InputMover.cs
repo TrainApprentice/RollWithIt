67d649a29456c5bee8c1262f9a09f9909acc580b
Danin <tenerid@ferris.edu>
2020-03-03 18:35:35 -0500

    Prefabs and Obstacles
    Infinite spawning of prefabs and randomized obstacles

---
+++ Infinite Runner 3/Assets/InputMover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputMover : MonoBehaviour {
+
+    LineRenderer line;
+    InputManager.InputConfig playerController;
+
+	void Start () {
+        line = GetComponent<LineRenderer>();
+
+        playerController = InputManager.player1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        GetComponent<MeshRenderer>().material.color = playerController.OnJump(true) ? Color.yellow : Color.white;
+
+        Vector3 input = playerController.GetAxis1();
+
+        if (input.sqrMagnitude > 1) input.Normalize();
+
+        transform.position = input;
+        line.SetPosition(1, input);
+        
+    }
+}
