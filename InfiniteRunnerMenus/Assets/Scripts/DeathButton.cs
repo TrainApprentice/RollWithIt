e732f3de3fbb5627ceabc33fc27248d759fc18a2
Jared Ebels <ebelsj2@ferris.edu>
2020-03-03 19:31:03 -0500

    Merged Movement, Prefabs, Yeti AI
    It's a mess but *shrug*

---
+++ InfiniteRunnerMenus/Assets/Scripts/DeathButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class DeathButton : MonoBehaviour
+{
+    public void Die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
